95783dbb0b69c6d9b39b0516e61edefb8b0cef41
Z3RYX <zeryxofficial@gmail.com>
2023-04-27 02:16:46 +0200

    Added LevelMetaData record, validator for meta data, and partial compiler for levels

---
+++ TagURIT.Core/LevelCompiler.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagURIT.Core.Levels;
+
+namespace TagURIT.Core
+{
+    public class LevelCompiler
+    {
+        private const string FILE_EXT = "tab";
+        private readonly string _TagPath;
+        public string TagPath { get { return _TagPath; } }
+
+        public LevelCompiler(string tagPath)
+        {
+            _TagPath = tagPath;
+        }
+
+        public void CompileLevel(LevelMetaData levelData)
+        {
+            if (!LevelMetaData.ValidateMetaData(levelData, out string? errorMsg)) throw new ArgumentException(errorMsg);
+
+            //TODO: Save compiled level in temp folder and return that as a path or stream
+            using var file = new FileStream($"{levelData.Name!.Replace(" ", "_")}.{FILE_EXT}", FileMode.Open);
+            using var archive = new ZipArchive(file, ZipArchiveMode.Update);
+
+            var metaFile = archive.CreateEntry("meta.ini");
+            using (var writer = new StreamWriter(metaFile.Open()))
+            {
+                writer.WriteLine("levelname=" + levelData.Name.Trim());
+                writer.WriteLine("description=" + (levelData.Description == null ? "" : levelData.Description.Trim()));
+                writer.WriteLine("author=" + levelData.Author!.Trim());
+                writer.WriteLine("created=" + levelData.CreationTime);
+                writer.WriteLine("lastupdated=" + levelData.LastUpdated);
+                writer.WriteLine("version=" + levelData.Version);
+            }
+
+            if (levelData.ThumbnailPath != null)
+            {
+                archive.CreateEntryFromFile(
+                    levelData.ThumbnailPath.ToString(), // Source file
+                    "thumbnail" + new FileInfo(levelData.ThumbnailPath.ToString()).Extension // Destination
+                );
+            }
+
+            /* TODO:
+             * Add level file to the archive
+             * Check level file for all assets it depends on
+             * Create list of assets that don't ship with the game
+             * Add those assets to the archive
+             */
+        }
+    }
+}
---
+++ TagURIT.Core/Levels/LevelMetaData.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagURIT.Core.Levels
+{
+    public record LevelMetaData
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? Author { get; set; }
+        public Uri? ThumbnailPath { get; set; }
+        public DateTime CreationTime { get; set; }
+        public DateTime LastUpdated { get; set; }
+        public int Version { get; set; }
+
+        public static bool ValidateMetaData(LevelMetaData data, out string? error)
+        {
+            var validImageFormats = new string[] { "png", "jpg", "jpeg", "bmp", "gif", "webp" };
+            int maxImageFileSize = 1024 * 1024 * 10; // 10 Megabytes
+
+            if (data == null)
+            {
+                error = "Data object cannot be null.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(data.Name))
+            {
+                error = "Level name cannot be null, empty, or only consisting of whitespace characters.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(data.Description) && data.Description != null)
+            {
+                error = "Level description cannot be empty or only consisting of whitespace characters. Use null instead.";
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(data.Author))
+            {
+                error = "Author name cannot be null, empty, or only consisting of whitespace characters.";
+                return false;
+            }
+
+            if (data.ThumbnailPath != null)
+            {
+                if (!File.Exists(data.ThumbnailPath.ToString()))
+                {
+                    error = $"Thumbnail does not exist at the specified path: {data.ThumbnailPath}";
+                    return false;
+                }
+
+                if (!data.ThumbnailPath.IsFile)
+                {
+                    error = $"The specified path for the thumbnail does not point to a file: {data.ThumbnailPath}";
+                    return false;
+                }
+
+                if (!validImageFormats.Contains(data.ThumbnailPath.AbsolutePath.Split('.').Last()))
+                {
+                    error = $"Thumbnail is not one of the allowed image file formats: " + string.Join(", ", validImageFormats);
+                    return false;
+                }
+
+                if (new FileInfo(data.ThumbnailPath.ToString()).Length > maxImageFileSize)
+                {
+                    error = $"Thumbnail cannot be bigger than {maxImageFileSize} bytes. Current size: {new FileInfo(data.ThumbnailPath.ToString()).Length}";
+                    return false;
+                }
+
+                //TODO: Verify image data and image aspect ratio (should be between 1:1 and 2:1)
+            }
+
+            if (data.CreationTime > DateTime.UtcNow)
+            {
+                error = "Creation time cannot be set in the future.";
+                return false;
+            }
+
+            if (data.LastUpdated > DateTime.UtcNow)
+            {
+                error = "Time of last update cannot be set in the future.";
+                return false;
+            }
+
+            if (data.LastUpdated < data.CreationTime)
+            {
+                error = "Time of last update cannot be before creation time.";
+                return false;
+            }
+
+            if (data.Version < 1)
+            {
+                error = "Level version cannot be lower than 1";
+                return false;
+            }
+
+            error = null;
+            return true;
+        }
+    }
+}
